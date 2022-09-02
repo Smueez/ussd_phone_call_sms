53256049e0c5b761090a8f727790d3e475ce409b
sheersho mueez <31783611+Smueez@users.noreply.github.com>
2022-09-02 15:43:06 +0600

    android activity fixes

--- android/src/main/java/com/example/ussd_phone_call_sms/UssdPhoneCallSmsPlugin.java
+++ android/src/main/java/com/example/ussd_phone_call_sms/UssdPhoneCallSmsPlugin.java
@@ -12,23 +12,21 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 
-import io.flutter.embedding.engine.plugins.activity.ActivityAware;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
-import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
 /** UssdPhoneCallSmsPlugin */
-public class UssdPhoneCallSmsPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware  {
+public class UssdPhoneCallSmsPlugin implements FlutterPlugin, MethodCallHandler  {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private MethodChannel channel;
   String TAG = "ussd_phone_call_sms";
-  private Activity activity;
+
   private Context context;
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
@@ -102,23 +100,4 @@ public class UssdPhoneCallSmsPlugin implements FlutterPlugin, MethodCallHandler,
     channel.setMethodCallHandler(null);
   }
 
-  @Override
-  public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
-    activity = binding.getActivity();
-  }
-
-  @Override
-  public void onDetachedFromActivityForConfigChanges() {
-
-  }
-
-  @Override
-  public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
-    activity = binding.getActivity();
-  }
-
-  @Override
-  public void onDetachedFromActivity() {
-
-  }
 }
